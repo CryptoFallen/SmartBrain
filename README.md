619dc69a117f21e343f73e44d9dc1f015c0db909
CryptoFallen <richardtom1203@protonmail.com>
2022-07-30 10:01:19 +0200

    Added in particle feature, description of how to use tsparticles (https://www.youtube.com/watch?v=u8y2vCkZubk), modification was also done to remove background of the particles to allow for visibility of main site

--- README.md
+++
@@ -1,2 +0,0 @@
-# SmartBrain
-SmartBrain project that detects facial recognition of images from ZTM course utilising React
--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -1,31 +1,18 @@
-import React, { Component } from 'react'
-// import Particles from "react-tsparticles";
-// import { loadFull } from "tsparticles";
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
-import './App.css';
+import React, { Component } from "react";
+import Particle from "./components/Particles/Particle";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import "./App.css";
 
-// const particleOptions = {
-//   particles: {
-//     line_linked: {
-//       shadow: {
-//         enabled: true,
-//         color: '#3CA9D1',
-//         blur: 5
-//       }
-//     }
-//   }
-// }
+
 
 class App extends Component {
-  render () {
+  render() {
     return (
       <div className="App">
-        {/* <Particles className='particles'
-         params={particleOptions}
-        /> */}
+        <Particle className="particles"/>
         <Navigation />
         <Logo />
         <Rank />
